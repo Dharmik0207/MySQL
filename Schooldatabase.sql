95d039588158080a2476b6f045debaea295f79c8
Code.With.Dharmik <dharmikgadhiya0207@gmail.com>
2025-10-11 00:24:07 +0200

    Add files via upload

---
+++ Schooldatabase.sql
@@ -0,0 +1,16 @@
+ -- CREATE DATABASE schooldatabase;
+ 
+ -- USE schooldatabase;
+ 
+ -- CREATE TABLE student ( 
+-- id INT PRIMARY KEY,  
+-- name VARCHAR(100),  
+-- age INT,  
+-- grade VARCHAR(10), 
+-- date_of_birth DATE
+--  );
+
+-- INSERT INTO student (id, name, age, grade, date_of_birth) 
+-- VALUES (1, "DHARMIK", 26, "1st", "2000-04-25"); 
+
+SELECT * FROM student;
